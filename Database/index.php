8bb3829b22387d3adc070184a3d0020958c70096
Roy Alexander Daae <super.atx@gmail.com>
2019-12-10 17:19:29 +0100

    Add files via upload

---
+++ Database/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="no" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>ER-modell</title>
+  </head>
+  <body>
+
+    <img src="logisk_ER-modell.jpg" alt="ER-modell av databasen" width="85%">
+
+  </body>
+</html>
